07a34e11085f33a4252ca1bc8d1933b36b212227
张史小冰风 <61115932+Zhangshibf@users.noreply.github.com>
2022-06-30 11:46:32 +0200

    Add files via upload

--- Text Preprocessing/main.py
+++ Text Preprocessing/main.py
@@ -1,5 +1,6 @@
 import pre_processing as p
 import sys
+import os
 #wikipedia Cantonese data
 
 opener = p.file_opener()
@@ -17,3 +18,12 @@ if __name__ == "__main__":
         c_scraped_cleaned = preprocessor.pre_process(opener.scraped_texts)
         for name,text in zip(opener.scraped_names,c_scraped_cleaned):
             p.save_monolingual_sentences(resource_name = name,path= sys.argv[3],sentences = text)
+
+    elif sys.argv[1]=="wiki_mandarin":
+        print("Text preprocessing might take very long time. Please be patient.")
+        opener.open_wiki_files(path = sys.argv[2])
+        m_cleaned = preprocessor.pre_process_wiki(opener.wiki_texts,language = "m")
+        for name, text in zip(opener.wiki_names, m_cleaned):
+            p.save_monolingual_sentences(resource_name=name, path=sys.argv[3], sentences=text)
+    else:
+        print("There is something wrong with your second argument.")
