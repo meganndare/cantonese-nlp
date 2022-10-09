678272f4715b6b5cb8d9358d4952edc2c2f4a033
Ewanwong <56022856+Ewanwong@users.noreply.github.com>
2022-10-09 14:13:47 +0200

    Add files via upload
    baseline_model:character_conversion

---
+++ character_conversion.py
@@ -0,0 +1,17 @@
+import opencc
+
+converter1 = opencc.OpenCC('hk2s.json')
+converter2 = opencc.OpenCC('t2s.json')
+
+read_file = 'test/test_cantonese_cross_char.txt'
+write_file = 'test/test_cantonese_simplified.txt'
+
+line_num = 0
+with open(read_file, 'r', encoding='utf-8') as f1:
+    with open(write_file, 'w', encoding='utf-8') as f2:
+        for line in f1.readlines():
+            hk_line = converter2.convert(converter1.convert(line))
+            f2.write(hk_line)
+            line_num += 1
+            if line_num % 1000 == 0:
+                print(str(line_num)+" sentences have been converted.")
